6bf2e5c5cdbfa338c373f3ed79358ddd70f9b6a2
Múria de Oliveira Abrantes <muria.abrantees@gmail.com>
2022-08-23 11:07:10 -0300

    resolução exercícios 1 ao 4 de 11 - bloco 4.1.

---
+++ fundamentos/bloco-4-java-script/dia-1-javascript-primeiros-passos/exercicio2de-11/script.js
@@ -0,0 +1,13 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const a = Math.floor(10 * Math.random() + 1);
+const b = Math.floor(10 * Math.random() + 1);
+console.log(" a = " + a, "b = " + b)
+
+if (a > b) {
+  console.log(" a é o maior.")
+} else {
+  console.log("b é o maior.")
+}
+
+// RESOLVIDO em 23 de ago as 00:42min!
