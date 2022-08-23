de9d61dcbc1600dc04213484bcb7ca4badbafce0
Múria de Oliveira Abrantes <muria.abrantees@gmail.com>
2022-08-23 00:43:34 -0300

    Exercicio 1de 11: cinco programas, um para cada operação aritmética básica.

---
+++ fundamentos/bloco-4-java-script/dia-1-javascript-primeiros-passos/exercicio1de-11/script.js
@@ -0,0 +1,22 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+const a = Math.floor(10 * Math.random() + 1);
+const b = Math.floor(10 * Math.random() + 1);
+console.log(a , b)
+// console.log(b)
+// Adição (a + b)
+console.log("Resultado da soma de a e b: " + (a + b))
+
+// Subtração (a - b)
+console.log("Resultado da subtração de a e b: " + (a - b))
+
+// Multiplicação (a * b)
+console.log("Resultado da Multiplicação de a e b: " + (a * b))
+
+// Divisão (a / b)
+console.log("Resultado da Divisão de a e b: " + (a / b))
+
+// Módulo (a % b)
+console.log("Resultado do Módulo a e b: " + (a % b))
+
+// RESOLVIDO em 23 de ago as 00:42min!
