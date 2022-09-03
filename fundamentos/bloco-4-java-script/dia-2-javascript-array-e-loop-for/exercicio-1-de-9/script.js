6d2b423164223c10389db4fac58d41a898efc256
Múria de Oliveira Abrantes <muria.abrantees@gmail.com>
2022-09-03 02:01:57 -0300

    Resolução exercicios a ao 4

---
+++ fundamentos/bloco-4-java-script/dia-2-javascript-array-e-loop-for/exercicio-1-de-9/script.js
@@ -0,0 +1,18 @@
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let numeros of numbers) {
+  console.log(numeros)
+}
+
+// retorno no console : 5
+// 9
+// 3
+// 19
+// 70
+// 8
+// 100
+// 2
+// 35
+// 27
---
+++ fundamentos/bloco-4-java-script/dia-2-javascript-array-e-loop-for/exercicio-5-de-9/script.js
@@ -0,0 +1,15 @@
+//  Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let numero;
+// let maiorNumber;
+// for (let index = 0; index < numbers.length; index += 1) {
+//   numero = numbers[index].length;
+//   if (numbers[index] > numero) {
+//     console.log(maiorNumber)
+//   } else {
+//     index = 0;
+//   }
+// }
+// console.log(maiorNumber)
