bd0f09e2d964d283835e4d967f20e8acd709de8e
Múria de Oliveira Abrantes <muria.abrantees@gmail.com>
2022-09-03 23:29:20 -0300

    exluindo branchs locais.

--- fundamentos/bloco-4-java-script/dia-2-javascript-array-e-loop-for/exercicio-8-de-9/script.js
+++ fundamentos/bloco-4-java-script/dia-2-javascript-array-e-loop-for/exercicio-8-de-9/script.js
@@ -1,9 +1,8 @@
 // Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
 let sabores = [];
 for (index = 0; index < 26; index = +1) {
- let numero = 0
- numero = sabores[index];
- sabores = numero + 1;
+ let numero = 0 +1;
+elemento =  sabores = numero + 1;
 
 }
 console.log(numero)
